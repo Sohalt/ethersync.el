e39b94654414f344e83b63869abffc4b496be499
sohalt <sohalt@sohalt.net>
2024-08-09 11:34:37 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.ethersync.url = "github:ethersync/ethersync";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ethersync,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with ethersync.packages.${system}; [ethersync];
+      };
+    });
+}
