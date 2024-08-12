04907475b0eba034818c014e1c4f43e5e1b5927e
sohalt <sohalt@sohalt.net>
2024-08-12 22:41:12 +0200

    update

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.default = pkgs.mkShell {
-        packages = [ethersync.packages.${system}.ethersync];
+        packages = let ep = ethersync.packages.${system}; in [ep.ethersync ep.neovim];
       };
     });
 }
